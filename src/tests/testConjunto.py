eaaef4ef4fb66bf2b808120fb14d8478a69adbaf
LAB_E502 <labe502@continental.edu.pe>
2025-04-23 11:23:46 -0500

    Correccion realizada

--- src/tests/testConjunto.py
+++ src/tests/testConjunto.py
@@ -1,7 +1,7 @@
 import unittest
-from  src.logica.conjunto import Conjunto
+from src.logica.conjunto import Conjunto
 
-class TestConjunto( unittest.TestCase ):
+class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
